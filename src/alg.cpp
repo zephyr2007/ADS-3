069f92624d3767f6fe9e163e22498fb3ea464b9c
zephyr2007 <amir.karimov.0418@gmail.com>
2026-04-05 16:53:06 +0300

    KARimov.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,17 +1,53 @@
 // Copyright 2025 NNTU-CS
-#include <cstdint>
 #include "alg.h"
+#include <cstdint>
 
 uint64_t collatzMaxValue(uint64_t num) {
-  return 1;
+  uint64_t max = num;
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num = num / 2;
+    } else {
+      num = num * 3 + 1;
+    }
+    if (num > max) {
+      max = num;
+    }
+  }
+  return max;
 }
 
 unsigned int collatzLen(uint64_t num) {
-  return 1;
+  unsigned int count = 1;
+  while (num != 1) {
+    if (num % 2 == 0) {
+      num = num / 2;
+    } else {
+      num = 3 * num + 1;
+    }
+    ++count;
+  }
+  return count;
 }
 
-unsigned int seqCollatz(unsigned int *maxlen,
-                        uint64_t lbound,
+unsigned int seqCollatz(unsigned int *maxlen, uint64_t lbound,
                         uint64_t rbound) {
-  return 1;
+  if (lbound > rbound) {
+    *maxlen = 0;
+    return 0;
+  }
+
+  unsigned int maxLen = 0;
+  uint64_t result = lbound;
+
+  for (uint64_t num = lbound; num <= rbound; ++num) {
+    unsigned int len = collatzLen(num);
+    if (len > maxLen) {
+      maxLen = len;
+      result = num;
+    }
+  }
+
+  *maxlen = maxLen;
+  return result;
 }
